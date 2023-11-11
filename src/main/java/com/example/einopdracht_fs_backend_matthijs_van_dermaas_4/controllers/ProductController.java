125fac68fa91e7e27d83a77a861a1bef91143fe0
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-11-11 16:35:46 +0100

    boder fix

---
+++ src/main/java/com/example/einopdracht_fs_backend_matthijs_van_dermaas_4/controllers/ProductController.java
@@ -0,0 +1,51 @@
+package com.example.einopdracht_fs_backend_matthijs_van_dermaas_4.controllers;
+
+
+
+import com.example.einopdracht_fs_backend_matthijs_van_dermaas_4.modelen.Product;
+import com.example.einopdracht_fs_backend_matthijs_van_dermaas_4.Dtos.ProductDto;
+import com.example.einopdracht_fs_backend_matthijs_van_dermaas_4.services.ProductService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts() {
+        List<ProductDto> productDto = productService.getAllProducts();
+        return new ResponseEntity<>(productDto, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getOneProduct(@PathVariable Long id) {
+        ProductDto productDto = productService.getProduct(id);
+        return new ResponseEntity<>(productDto, HttpStatus.OK);
+    }
+
+
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
+        ProductDto newProduct = productService.createProduct(productDto);
+        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductDto> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
---
+++ src/test/java/com/example/einopdracht_fs_backend_matthijs_van_dermaas_4/services/ProductServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.einopdracht_fs_backend_matthijs_van_dermaas_4.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void getProduct() {
+    }
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void testSaveProduct() {
+    }
+
+    @Test
+    void processSqlFile() {
+    }
+}

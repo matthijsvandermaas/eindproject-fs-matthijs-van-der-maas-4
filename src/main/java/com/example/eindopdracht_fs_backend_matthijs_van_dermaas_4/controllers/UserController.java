b52c18d178642835695a009e08a00b9875e1e11f
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2023-11-14 17:48:38 +0100

    Merge remote-tracking branch 'origin/branch_security_opnieuw_gemaakt-4.6.3' into branch_security_opnieuw_gemaakt-4.6.3
    # Conflicts:
    #	src/main/resources/data.sql

---
+++ src/main/java/com/example/eindopdracht_fs_backend_matthijs_van_dermaas_4/controllers/UserController.java
@@ -0,0 +1,65 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.controllers;
+
+
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.Dtos.ProfileAndUserDto;
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.Dtos.UserDto;
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.modelen.User;
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/createWithProfile")
+    public ResponseEntity<UserDto> createUserWithProfile(@Valid @RequestBody ProfileAndUserDto profileDto) {
+        UserDto result = userService.createUserWithProfile(profileDto);
+        System.out.println("Received request: " + profileDto.toString());
+        return new ResponseEntity<>(result, HttpStatus.CREATED);
+    }
+
+
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> dDto = userService.getAllUsers();
+        return new ResponseEntity<>(dDto, HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        System.out.println("Request received for user with id: " + id);
+        Optional<User> user = userService.getUserById(id);
+
+        if (user.isPresent()) {
+            System.out.println("User found: " + user.get());
+            return new ResponseEntity<>(user.get(), HttpStatus.OK);
+        } else {
+            System.err.println("User not found with id: " + id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+
+
+}
+
---
+++ src/test/java/com/example/eindopdracht_fs_backend_matthijs_van_dermaas_4/controllers/UserControllerTest.java
@@ -0,0 +1,93 @@
+package com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.controllers;
+
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.Dtos.ProfileAndUserDto;
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.Dtos.UserDto;
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.Security.JwtService;
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.modelen.User;
+import com.example.eindopdracht_fs_backend_matthijs_van_dermaas_4.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@WebMvcTest(UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    public JwtService jwtService;
+
+    @MockBean
+    public UserDetailsService userDetailsService;
+
+    @MockBean
+    public PasswordEncoder passwordEncoder;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Test
+    void testCreateUserWithProfile() throws Exception {
+        ProfileAndUserDto profileDto = new ProfileAndUserDto();
+        profileDto.setPassword("testPassword");
+
+        when(passwordEncoder.encode(eq("testPassword"))).thenReturn("encodedPassword");
+        when(userService.createUserWithProfile(profileDto)).thenReturn(new UserDto());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/users/createWithProfile")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{}"))
+                .andExpect(status().isCreated());
+
+        verify(passwordEncoder, times(1)).encode(eq("testPassword"));
+        verify(userService, times(1)).createUserWithProfile(profileDto);
+    }
+
+    @Test
+    void testGetUserById() throws Exception {
+        Long userId = 1L;
+        User mockUser = new User();
+        mockUser.setId(userId);
+        mockUser.setPassword("testPassword");
+        mockUser.setUsername("username");
+
+        when(userService.getUserById(userId)).thenReturn(Optional.of(mockUser));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", userId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.id").value(userId))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.password").value("testPassword"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.username").value("username"));
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/users/{id}", userId)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        System.out.println(result.getResponse().getContentAsString());
+    }
+}
